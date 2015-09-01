32563a64aeed5f809fd64e4de11d83b681bc28ad
vivit-phruktayanon <vivitphruktayanon@gmail.com>
2015-09-01 13:50:03 +0700

    commit lab1-2

---
+++ Lab201/Student.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Student
+    {
+        public string Name;
+        public string StudentID;
+
+
+        private int years = 0;
+        
+        public int YearOfBirth
+        {
+            get { return years; }
+
+
+            set
+            {
+                if (value >= 1950 && value <= 2000)
+                {
+                    years = value;
+
+                }
+                else 
+                {
+                    Console.WriteLine("{0}:error try setting invalid year-of-birth value!", StudentID);
+                }
+                
+            }   
+        }
+        public bool isActive;
+        public Student()
+        {
+            this.Name = "John Doe";
+            this.StudentID = "Unknown";
+            this.YearOfBirth = 1995;
+            this.isActive = false;
+        }
+        public Student(string Name, string StudentID)
+        {
+            this.Name = Name;
+            this.StudentID = StudentID;
+            this.YearOfBirth = 1995;
+            this.isActive = true;
+        }
+        public Student(string Name, string StudentID, int YearOfBirth)
+        {
+            this.Name = Name;
+            this.StudentID = StudentID;
+            this.YearOfBirth = YearOfBirth;
+            this.isActive = true;
+        }
+        public Student(string Name, string StudentID, int YearOfBirth, bool isActive)
+        {
+            this.Name = Name;
+            this.StudentID = StudentID;
+            this.YearOfBirth = YearOfBirth;
+            this.isActive = isActive;
+        }
+
+        public int getAge()
+        {
+                return DateTime.Today.Year - YearOfBirth;
+        }
+        public override string ToString()
+        {
+            if (isActive == true)
+            {
+                return "[Studen: " + Name + " (" + StudentID + ")" + ", age=" + getAge() + ", is active student]";
+            }
+            else
+            {
+                return "[Studen: " + Name + " (" + StudentID + ")" + ", age=" + getAge() + ", is NOT active student]";
+            }
+        }
+    }
+}
---
+++ Lab202/Course.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Course
+    {
+        private string course = "";
+        private int max = 0;
+        private int num = 0;
+        public string Name;
+        public string CourseID
+        {
+            get { return course; }
+            set
+            {
+                if (value.Length == 6 && value != CourseID)
+                {
+                    course = value;
+                }
+                else
+                    Console.WriteLine("{0}: error try setting invalid CourseID!", CourseID);
+            }
+
+        }
+        public string Lecturer;
+        public int MaxStudents
+        {
+            get { return max; }
+            set
+            {
+                if (value >= 0 && value <= 80 && value >= NumStudents)
+                    max = value;
+                else
+                    Console.WriteLine("{0}: error try setting invalid Max No. Students!", CourseID);
+            }
+        }
+        public int NumStudents
+        {
+            get { return num; }
+            set
+            {
+                if (value >= 0 && value <= MaxStudents)
+                    num = value;
+                else
+                    Console.WriteLine("{0}: error try setting invalid No. Students!", CourseID);
+            }
+        }
+
+        public Course()
+        {
+            this.Name = "";
+            this.CourseID = "";
+            this.Lecturer = "staff";
+            this.MaxStudents = 30;
+            this.NumStudents = 0;
+        }
+
+        public Course(string Name, string CourseID)
+        {
+            this.Name = Name;
+            this.CourseID = CourseID;
+            this.Lecturer = "staff";
+            this.MaxStudents = 30;
+            this.NumStudents = 0;
+        }
+
+        public Course(string Name, string CourseID, string Lecturer)
+        {
+            this.Name = Name;
+            this.CourseID = CourseID;
+            this.Lecturer = Lecturer;
+            this.MaxStudents = 30;
+            this.NumStudents = 0;
+        }
+        public Course(string Name, string CourseID, string Lecturer, int MaxStudents)
+        {
+            this.Name = Name;
+            this.CourseID = CourseID;
+            this.Lecturer = Lecturer;
+            this.MaxStudents = MaxStudents;
+            this.NumStudents = 0;
+        }
+        public override string ToString()
+        {
+            string s = "[Course: " + Name + " (" + CourseID + "), Lecturer=" + Lecturer + ", has " + NumStudents + " students, max=" + MaxStudents + "]";
+            return s;
+        }
+    }
+}
